5b45071881a25e01157f27e8ef83be231240f9fc
bilql-sinqv <47627384+bilql-sinqv@users.noreply.github.com>
2022-11-14 05:56:10 +0300

    Update Gorev 1.cpp

--- Gorev 1.cpp
+++ Gorev 1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#define g 9.80665
 using namespace std;
 
 int main() {
 	float t;
-	double g = 9.80665;
 	cout << "Cisim kacinci saniyede? : ";
 	cin >> t;
 	cout << "\n Cismin aldigi toplam yol : " << 0.5*g*t*t << " metre";
