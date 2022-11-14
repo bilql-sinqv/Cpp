f653ef22e8a412a2e77700b29c4cae7ab2687cff
bilql-sinqv <47627384+bilql-sinqv@users.noreply.github.com>
2022-11-14 05:39:23 +0300

    Update Hello Cpp.cpp

--- Hello Cpp.cpp
+++ Hello Cpp.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
-	cout << "Hello cpp\n";
+	float t;
+	double g = 9.80665;
+	cout << "Cisim kacinci saniyede? : ";
+	cin >> t;
+	cout << "\n Cismin aldigi toplam yol : " << 0.5*g*t*t << " metre";
 	return 0;
 }
